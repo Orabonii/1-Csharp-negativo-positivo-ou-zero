8f6c6442bb95cae20c62bac28b41e6524317f01d
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-06-04 12:27:15 -0300

    Add files via upload

---
+++ exercicio02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro:");
+        
+        int numero = Convert.ToInt32(Console.ReadLine());
+        
+        if (numero > 0)
+        {
+            Console.WriteLine("O número inserido é positivo.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("O número inserido é negativo.");
+        }
+        else
+        {
+            Console.WriteLine("O número inserido é zero.");
+        }
+    }
+}
